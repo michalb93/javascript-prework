d5184b2a98140d1bedcdb2bc73c4f52e8f973c6b
Michal Borek <borekmichal30@gmail.com>
2020-02-25 21:54:32 +0100

    Add Game

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ function clearMessages(){
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+'';
