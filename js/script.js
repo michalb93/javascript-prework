725a2cdf3eebba00fe22abd8f936737d3176254f
Michal Borek <borekmichal30@gmail.com>
2020-02-26 19:28:19 +0100

    Add playermove

--- js/script.js
+++ js/script.js
@@ -12,19 +12,33 @@ var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
+var item, playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else if (playerInput > '3') {
+  printMessage('Wprowadzono złą liczbę, wybierz ponownie 1,2 lub 3.');
 }
-if (randomNumber == '2') {
-  computerMove = 'papier';
-} else if (randomNumber == '3') {
-  computerMove = 'nożyce';
+printMessage('Twój ruch: ' + playerMove);
+
+
+var item, playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else if (playerInput > '3') {
+  printMessage('Wprowadzono złą liczbę, wybierz ponownie 1,2 lub 3.');
 }
-printMessage('Mój ruch: ' + computerMove);
+printMessage('Twój ruch: ' + playerMove);
 
 '';
