f5cdc8d5d7aa27116099e3c2a0ba30647b3b4c22
Michal Borek <borekmichal30@gmail.com>
2020-03-04 13:46:58 +0100

    Add Fix Change

--- js/script.js
+++ js/script.js
@@ -1,53 +1,60 @@
 
 
-const buttonKamien = document.getElementById('kamien'),
-      buttonPapier = document.getElementById('papier'),
-	  buttonNozyce = document.getElementById('nozyce');
+		 const buttonKamien = document.getElementById('kamien'),
+				buttonPapier = document.getElementById('papier'),
+				buttonNozyce = document.getElementById('nozyce');
 
-function buttonClicked(argButtonName) {
-         clearMessages();
-         console.log(argButtonName + ' został kliknięty');
-		 playerMove = argButtonName;
-		 console.log('ruch gracza to: ' + playerMove);
-		 randomNumber = Math.floor(Math.random() * 3 + 1);
-		 console.log('wylosowana liczba to: ' + randomNumber);
-		 computerMove = getMoveName(randomNumber);
-		 console.log('ruch komputera to: ' + computerMove);
-		 displayResult(playerMove, computerMove);
+		 function buttonClicked(argButtonName) {
+			learMessages();
+			console.log(argButtonName + ' został kliknięty');
+			playerMove = argButtonName;
+			console.log('ruch gracza to: ' + playerMove);
+			randomNumber = Math.floor(Math.random() * 3 + 1);
+			console.log('wylosowana liczba to: ' + randomNumber);
+			computerMove = getMoveName(randomNumber);
+			console.log('ruch komputera to: ' + computerMove);
+			displayResult(playerMove, computerMove);
 		 }
 
-function getMoveName(argMoveId) {
-		 console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-		 if 		(argMoveId == 1) {
-		 return 'kamien';
-		 } else if (argMoveId == 2) {
-		 return 'papier';
-		 } else if (argMoveId == 3) {
-		 return 'nozyce';
-		 } else {
-		 printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamien".');
+		 function getMoveName(argMoveId) {
+			console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+		 if(argMoveId == 1){
+		 	return 'kamien';
+		 } 
+		 else if (argMoveId == 2){
+			return 'papier';
+		 } 
+		 else if (argMoveId == 3){
+			return 'nozyce';
+		 } 
+		 else {
+			printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamien".');
 		 return 'kamien';
 		}
 	} 
 
 
-function displayResult(argPlayerMove, argComputerMove) {
-		 console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-		 if 		(argPlayerMove == 'papier' && argComputerMove == 'kamien') {
-		 printMessage('Wygrywasz!');
-		 } else if (argPlayerMove == 'kamien' && argComputerMove == 'nozyce') {
-		 printMessage('Wygrywasz!');
-		 } else if (argPlayerMove == 'nozyce' && argComputerMove == 'papier') {
-		 printMessage('Wygrywasz!');
-		 } else if (argPlayerMove == argComputerMove) {
-		 printMessage('Remis');
-		 } else {
-		 printMessage('Przegrywasz :(');
-		}
-		 printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-		}
+		 function displayResult(argPlayerMove, argComputerMove) {
+			console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+		 if (argPlayerMove == 'papier' && argComputerMove == 'kamien'){
+			printMessage('Wygrywasz!');
+		 } 
+		 else if (argPlayerMove == 'kamien' && argComputerMove == 'nozyce'){
+			printMessage('Wygrywasz!');
+		 } 
+		 else if (argPlayerMove == 'nozyce' && argComputerMove == 'papier'){
+			printMessage('Wygrywasz!');
+		 } 
+		 else if (argPlayerMove == argComputerMove){
+			printMessage('Remis');
+		 } 
+		 else {
+			printMessage('Przegrywasz :(');
+		 }
+			printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+		 }
 
 
-		 buttonKamien.addEventListener('click', function(){ buttonClicked('kamien'); });
-		 buttonPapier.addEventListener('click', function(){ buttonClicked('papier'); });
-		 buttonNozyce.addEventListener('click', function(){ buttonClicked('nozyce'); });
+		 buttonKamien.addEventListener('click', function(){buttonClicked('kamien'); });
+		 buttonPapier.addEventListener('click', function(){buttonClicked('papier'); });
+		 buttonNozyce.addEventListener('click', function(){buttonClicked('nozyce'); });
